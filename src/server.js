8d3914317752e0306a85ed5e44dfdc72da1cf1d4
Carlos Santos <62728109+carlosantoss@users.noreply.github.com>
2020-06-04 15:36:33 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const server = express()
+
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+//configurar caminhos da minha aplicacao
+//pagina inicial
+//req: requisicao
+//res: resposta
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Um título"})
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
+
+//ligar o servidor
+server.listen(3000)
